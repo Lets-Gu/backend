eb79af542ca30c32a1d78d76c07a7191c345df23
Juhye0k <kyoung1678@naver.com>
2025-07-22 19:32:40 +0900

    feat : 초기 프로젝트 생성

---
+++ src/main/java/avengers/lion/global/exception/BusinessException.java
@@ -0,0 +1,13 @@
+package avengers.lion.global.exception;
+
+import lombok.Getter;
+@Getter
+public class BusinessException extends RuntimeException{
+
+    private final ExceptionType exceptionType;
+
+    public BusinessException(ExceptionType exceptionType){
+        super(exceptionType.getMessage());
+        this.exceptionType=exceptionType;
+    }
+}
---
+++ src/test/java/avengers/lion/LionApplicationTests.java
@@ -0,0 +1,13 @@
+package avengers.lion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
