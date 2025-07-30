348eda0d6ec21abe970184d51ef9a157dae19007
Juhye0k <kyoung1678@naver.com>
2025-07-30 20:21:24 +0900

    chore : 예외 타입 설정

--- src/main/java/avengers/lion/global/exception/ExceptionType.java
+++ src/main/java/avengers/lion/global/exception/ExceptionType.java
@@ -18,7 +18,11 @@ public enum ExceptionType {
 
     // Member
     MEMBER_NOT_FOUND(NOT_FOUND, "U001", "사용자를 찾을 수 없습니다."),
-    ;
+
+
+    //Auth
+    EXPIRED_REFRESH_TOKEN(UNAUTHORIZED,"A001","리프레시 토큰이 만료되었습니다."),
+    INVALID_REFRESH_TOKEN(UNAUTHORIZED, "A002","리프레시 토큰이 유효하지 않습니다");
 
     private final HttpStatus status;
     private final String code;
