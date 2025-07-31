70a458ad089889d9fcab850fc58877120e99b068
Juhye0k <kyoung1678@naver.com>
2025-07-31 20:54:20 +0900

    feat : 리뷰 도메인

---
+++ src/main/java/avengers/lion/review/domain/Review.java
@@ -0,0 +1,33 @@
+package avengers.lion.review.domain;
+
+import avengers.lion.global.base.BaseEntity;
+import avengers.lion.member.Member;
+import avengers.lion.mission.domain.Mission;
+import jakarta.persistence.*;
+import lombok.NoArgsConstructor;
+
+@Entity
+@NoArgsConstructor
+public class Review extends BaseEntity {
+
+    @Id @GeneratedValue
+    @Column(name = "review_id")
+    private Long reviewId;
+
+    @Column(name = "title", nullable = false)
+    private String title;
+
+    @Column(name = "content", nullable = false)
+    private String content;
+
+    @Column(name = "image_url")
+    private String imageUrl;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id")
+    private Member member;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "mission_id")
+    private Mission mission;
+}
