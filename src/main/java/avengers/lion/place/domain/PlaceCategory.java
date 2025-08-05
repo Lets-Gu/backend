39273b8f2dce57ab930590fab8ca66dbbee20663
Juhye0k <kyoung1678@naver.com>
2025-08-05 15:37:36 +0900

    feat : 장소 카테고리 설정

--- src/main/java/avengers/lion/place/domain/PlaceCategory.java
+++ src/main/java/avengers/lion/place/domain/PlaceCategory.java
@@ -5,7 +5,11 @@ import lombok.Getter;
 @Getter
 public enum PlaceCategory {
 
-    RESTAURANT("음식점");
+    CULTURE_HISTORY("문화/역사"),
+    NATURE_PARK("자연/공원"),
+    FOOD_CAFE("음식/카페"),
+    ART_EXHIBITION_EXPERIENCE("예술/전시/체험"),
+    LIFE_CONVENIENCE("생활/편의시설");
 
     private final String name;
 
