f39b440bd96ff2e77688a056b6c21b1c24ce34c0
Juhye0k <kyoung1678@naver.com>
2025-08-05 15:36:44 +0900

    chore : 필요없는 dto 삭제

--- src/main/java/avengers/lion/place/dto/GeocodingResult.java
+++
@@ -1,9 +0,0 @@
-package avengers.lion.place.dto;// 3) GeocodingResult.java
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-
-@JsonIgnoreProperties(ignoreUnknown = true)
-public record GeocodingResult(
-        String formatted_address,
-        double lat,
-        double lng
-) {}
