f4d02b2376fce2072f1bb5cd94883d25cd593a89
Juhye0k <kyoung1678@naver.com>
2025-08-06 18:38:44 +0900

    feat : GPS 인증 구현

--- src/main/java/avengers/lion/mission/controller/MissionController.java
+++ src/main/java/avengers/lion/mission/controller/MissionController.java
@@ -2,16 +2,14 @@ package avengers.lion.mission.controller;
 
 import avengers.lion.global.response.ResponseBody;
 import avengers.lion.global.response.ResponseUtil;
+import avengers.lion.mission.dto.GpsAuthenticationRequest;
 import avengers.lion.mission.dto.MissionResponse;
 import avengers.lion.mission.dto.MissionReviewResponse;
 import avengers.lion.mission.service.MissionService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -35,17 +33,18 @@ public class MissionController {
     미션에 대한 리뷰 전체조회
      */
     @GetMapping("/{missionId}/reviews")
-    public ResponseEntity<ResponseBody<List<MissionReviewResponse>>> getMissionReview(@PathVariable Long missionId){
+    public ResponseEntity<ResponseBody<List<MissionReviewResponse>>> getMissionReviews(@PathVariable Long missionId){
         return ResponseEntity.ok(ResponseUtil.createSuccessResponse(missionService.getMissionReviews(missionId)));
     }
 
-    //TODO : 인증 완료되면 개발하기
     /*
      미션 인증하기  -> gps 인증
      */
-
-    /*
-    사진 인증
-     */
+    @PostMapping("/{missionId}/gps")
+    public ResponseEntity<ResponseBody<Void>> gpsAuthentication(@PathVariable Long missionId, @RequestBody GpsAuthenticationRequest gpsAuthenticationRequest){
+        missionService.GpsAuthentication(missionId, gpsAuthenticationRequest);
+        return ResponseEntity.ok(ResponseUtil.createSuccessResponse());
+    }
+    //TODO: 사진 인증  SSE 구현
 
 }
