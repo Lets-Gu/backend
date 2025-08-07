1476fcc9a3039c1398222db2c4b1bb46a2aed264
Juhye0k <kyoung1678@naver.com>
2025-08-07 19:19:14 +0900

    feat : 아이템 api

---
+++ src/main/java/avengers/lion/item/dto/ItemResponse.java
@@ -0,0 +1,16 @@
+package avengers.lion.item.dto;
+
+
+import avengers.lion.item.domain.Item;
+
+public record ItemResponse(Long itemId, String itemName, int price, int count) {
+
+    public static ItemResponse of(Item item){
+        return new ItemResponse(
+                item.getId(),
+                item.getName(),
+                item.getPrice(),
+                item.getStockCount()
+        );
+    }
+}
