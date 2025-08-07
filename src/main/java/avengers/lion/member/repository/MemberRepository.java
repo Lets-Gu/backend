5f1dd8eb00e0b69fce66f04b160997ea0989c2e0
Juhye0k <kyoung1678@naver.com>
2025-08-07 10:21:21 +0900

    feat : 마이페이지 내 프로필 조회

---
+++ src/main/java/avengers/lion/member/repository/MemberRepository.java
@@ -0,0 +1,9 @@
+package avengers.lion.member.repository;
+
+import avengers.lion.member.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface MemberRepository extends JpaRepository<Member, Long> {
+}
