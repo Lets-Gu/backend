d1ce77b74f9dc354df424c902bda92ca9f996ccd
Juhye0k <kyoung1678@naver.com>
2025-08-09 16:35:40 +0900

    chore : 날씨 Get 으로 수정

--- src/main/java/avengers/lion/weather/controller/WeatherController.java
+++ src/main/java/avengers/lion/weather/controller/WeatherController.java
@@ -8,9 +8,7 @@ import avengers.lion.weather.service.WeatherService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
@@ -18,7 +16,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class WeatherController implements WeatherApi {
     private final WeatherService weatherService;
 
-    @PostMapping
+    @GetMapping
     @PreAuthorize("hasAuthority('ROLE_USER')")
     public ResponseEntity<ResponseBody<WeatherBasic>> getWeather(){
         return ResponseEntity.ok(ResponseUtil.createSuccessResponse(weatherService.oneCall()));
