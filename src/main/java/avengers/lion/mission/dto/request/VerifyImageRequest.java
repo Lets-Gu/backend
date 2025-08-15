df2fb1058580e456bac86bc1305f675cc9944057
Juhye0k <kyoung1678@naver.com>
2025-08-15 19:51:58 +0900

    feat : uploadImageUrl 추가

--- src/main/java/avengers/lion/mission/dto/request/VerifyImageRequest.java
+++ src/main/java/avengers/lion/mission/dto/request/VerifyImageRequest.java
@@ -3,6 +3,9 @@ package avengers.lion.mission.dto.request;
 import jakarta.validation.constraints.NotBlank;
 
 public record VerifyImageRequest(
+    @NotBlank
+    String uploadImageUrl,
+
     @NotBlank(message = "이미지 URL은 필수입니다")
     String imageUrl,     // 업로드된 Cloudinary 이미지 URL
     
