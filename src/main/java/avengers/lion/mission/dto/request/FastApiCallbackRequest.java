7460904c70b77b4da6865a14be240c8d54c44d5e
Juhye0k <kyoung1678@naver.com>
2025-08-17 13:03:45 +0900

    chore: HMAC 서명 dto 수정

--- src/main/java/avengers/lion/mission/dto/request/FastApiCallbackRequest.java
+++ src/main/java/avengers/lion/mission/dto/request/FastApiCallbackRequest.java
@@ -2,10 +2,12 @@ package avengers.lion.mission.dto.request;
 
 import avengers.lion.mission.dto.VerificationEventType;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
 
 /*
 Fast Api로부터 받아오는 dto
  */
+@JsonPropertyOrder({"job_id", "event_type", "verified"})
 public record FastApiCallbackRequest(
         @JsonProperty("job_id") String jobId,
         @JsonProperty("event_type") VerificationEventType eventType,  // "progress" | "completed" | "failed"
