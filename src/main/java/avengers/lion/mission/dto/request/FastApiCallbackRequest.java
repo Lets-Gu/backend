abf2a15d062c9a2e13da6510554a2a83e2ad3e4d
juhyeok <kyoung1678@naver.com>
2025-08-17 12:18:28 +0900

    Update FastApiCallbackRequest.java

--- src/main/java/avengers/lion/mission/dto/request/FastApiCallbackRequest.java
+++ src/main/java/avengers/lion/mission/dto/request/FastApiCallbackRequest.java
@@ -8,6 +8,7 @@ Fast Api로부터 받아오는 dto
  */
 public record FastApiCallbackRequest(
         @JsonProperty("job_id") String jobId,
-        @JsonProperty("event_type") VerificationEventType eventType  // "progress" | "completed" | "failed"
+        @JsonProperty("event_type") VerificationEventType eventType,  // "progress" | "completed" | "failed"
+        Boolean verified
 ) {
 }
