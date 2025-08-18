1d5ac07b15d1ac8d6a74525293eb1660f782c3a8
Juhye0k <kyoung1678@naver.com>
2025-08-18 18:19:38 +0900

    refactor : 미션 생성 로직 수정

--- src/main/java/avengers/lion/mission/service/MissionScheduler.java
+++ src/main/java/avengers/lion/mission/service/MissionScheduler.java
@@ -5,12 +5,11 @@ import avengers.lion.global.exception.ExceptionType;
 import avengers.lion.mission.domain.BatchStatus;
 import avengers.lion.mission.domain.Mission;
 import avengers.lion.mission.domain.MissionBatches;
+import avengers.lion.mission.domain.MissionTemplate;
 import avengers.lion.mission.repository.MissionBatchRepository;
 import avengers.lion.mission.repository.MissionRepository;
-import avengers.lion.place.domain.Place;
+import avengers.lion.mission.repository.MissionTemplateRepository;
 import avengers.lion.place.domain.PlaceCategory;
-import avengers.lion.place.repository.PlaceRepository;
-import avengers.lion.place.service.PlaceService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.quartz.Job;
@@ -28,11 +27,9 @@ import java.util.List;
 @Slf4j
 public class MissionScheduler implements Job {
 
-    private final PlaceService placeService;
-    private final MissionGenerateService missionGenerateService;
     private final MissionRepository missionRepository;
     private final MissionBatchRepository missionBatchRepository;
-    private final PlaceRepository placeRepository;
+    private final MissionTemplateRepository missionTemplateRepository;
 
     @Override
     @Transactional
@@ -40,24 +37,26 @@ public class MissionScheduler implements Job {
 
         // 기존 배치 상태 업데이트
         finishMissionBatch();
+        MissionBatches newMissionBatches = startMissionBatch();
         // GPT에 보낼 미션 데이터들
-        List<Place> places = new ArrayList<>();
+        List<MissionTemplate> missions = new ArrayList<>();
         /*
         각 카테고리별로 조회
          */
         for(PlaceCategory category : PlaceCategory.values()){
             // 현재일 기준으로, 20일 전에 조회된 데이터는 제외
             LocalDateTime now = LocalDate.now().minusDays(20).atStartOfDay();
-            Place place = placeRepository.findByCategoryWithSelectionCriteria(category,now)
+            MissionTemplate missionTemplate = missionTemplateRepository.findByCategoryWithSelectionCriteria(category,now)
                     .orElseThrow(()->new BusinessException(ExceptionType.PLACE_NOT_FOUND));
             // 마지막으로 조회된 날짜, 카운트 개수 설정
-            place.updateSelectionInfo();
-            places.add(place);
+            missionTemplate.updateSelectionCount();
+            missions.add(missionTemplate);
         }
-        MissionBatches newMissionBatches = startMissionBatch();
         missionBatchRepository.save(newMissionBatches);
-        List<Mission> missions = missionGenerateService.generateMissionsFromPlaces(places, newMissionBatches);
-        missionRepository.saveAll(missions);
+        for(MissionTemplate missionTemplate : missions){
+            Mission mission = Mission.createFromTemplate(missionTemplate, newMissionBatches);
+            missionRepository.save(mission);
+        }
     }
 
     /*
