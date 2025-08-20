8b879cdb8d6e1d3ff2c554747a49f64ba1b80511
Juhye0k <kyoung1678@naver.com>
2025-08-20 21:13:48 +0900

    chore : 아이템 조회 시 이미지 주소 추가

--- src/main/java/avengers/lion/item/dto/ItemResponse.java
+++ src/main/java/avengers/lion/item/dto/ItemResponse.java
@@ -3,14 +3,15 @@ package avengers.lion.item.dto;
 
 import avengers.lion.item.domain.Item;
 
-public record ItemResponse(Long itemId, String itemName, int price, int count) {
+public record ItemResponse(Long itemId, String itemName, int price, int count, String imageUrl) {
 
     public static ItemResponse of(Item item){
         return new ItemResponse(
                 item.getId(),
                 item.getName(),
                 item.getPrice(),
-                item.getStockCount()
+                item.getStockCount(),
+                item.getImageUrl()
         );
     }
 }
