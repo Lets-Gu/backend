3d214a075ed2b61423747a2c8817620b3518ce6f
Juhye0k <kyoung1678@naver.com>
2025-08-22 23:49:07 +0900

    refactor : 상품 이미지 추가

--- src/main/java/avengers/lion/wallet/dto/GiftCardResponse.java
+++ src/main/java/avengers/lion/wallet/dto/GiftCardResponse.java
@@ -4,13 +4,14 @@ import avengers.lion.item.domain.OrderItem;
 
 import java.time.LocalDateTime;
 
-public record GiftCardResponse(Long itemId, String itemName, LocalDateTime createdAt) {
+public record GiftCardResponse(Long itemId, String itemName, LocalDateTime createdAt, String imageUrl) {
 
     public static GiftCardResponse of(OrderItem orderItem){
         return new GiftCardResponse(
             orderItem.getItem().getId(), 
             orderItem.getItem().getName(),
-                orderItem.getCreatedAt()
+                orderItem.getCreatedAt(),
+                orderItem.getItem().getImageUrl()
         );
     }
 }
--- src/main/java/avengers/lion/wallet/dto/ParentItemResponse.java
+++ src/main/java/avengers/lion/wallet/dto/ParentItemResponse.java
@@ -2,9 +2,9 @@ package avengers.lion.wallet.dto;
 
 import avengers.lion.item.domain.OrderItem;
 
-public record ParentItemResponse(Long itemId, String itemName) {
+public record ParentItemResponse(Long itemId, String itemName, String imageUrl) {
 
     public static ParentItemResponse of(OrderItem orderItem){
-        return new ParentItemResponse(orderItem.getItem().getId(), orderItem.getItem().getName());
+        return new ParentItemResponse(orderItem.getItem().getId(), orderItem.getItem().getName(), orderItem.getItem().getImageUrl());
     }
 }
