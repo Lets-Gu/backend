07c623b082c0b5cb57a59a755d5994c98f839bea
juhyeok <kyoung1678@naver.com>
2025-08-23 03:21:34 +0900

    Update ReviewController.java

--- src/main/java/avengers/lion/review/controller/ReviewController.java
+++ src/main/java/avengers/lion/review/controller/ReviewController.java
@@ -24,7 +24,7 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/v1/reviews")
-public class ReviewController implements ReviewApi{
+public class ReviewController {
 
     private final ReviewService reviewService;
 
